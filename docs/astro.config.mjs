79551f068bf52598e97ccbaaa13357e8a3ae8464
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-07-08 09:51:18 +0530

    docs: initialize astro starlight for documentation
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

---
+++ docs/astro.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+      starlight({
+          title: 'kiln',
+          logo: {
+              src: './src/assets/logo.svg',
+              replacesTitle: true,
+          },
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/thunderbottom/kiln' }],
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+          customCss: [
+            "./src/styles/global.css",  
+          ],
+      }),
+	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
